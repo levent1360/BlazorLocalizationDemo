4af0ab78cc267ef688d4d3149c318e35e194a3ff
levent1360 <levent1360@gmail.com>
2024-08-14 16:10:13 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@ using BlazorLocalizationDemo.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddLocalization();
+builder.Services.AddControllers();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
+
+string[] supportedCultures = ["en", "tr", "ru"];
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,6 +31,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.MapControllers();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
